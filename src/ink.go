cb417bb530edd971c1c11fe20d01c8c775916e3b
song <imeoer@gmail.com>
2014-10-30 23:38:58 +0800

    modify func define and fix router bug

--- src/ink.go
+++ src/ink.go
@@ -1,82 +1,76 @@
 package main
 
 import (
-    "strconv"
     "net/http"
     "strings"
     "regexp"
+    "os"
     "fmt"
 )
 
 /* data structure */
 
-type Response struct {
-    http.ResponseWriter
-    parms map[string]string
-}
-
-type Request struct {
-    http.Request
-}
-
 type MatchMap map[string]string
 
-type MiddlewareHandle func(w Response, r *Request, next func())
-type Handle func(w Response, r *Request)
+type Handle func(ctx Context)
 
-type Mux struct {
+type Context struct {
+    http.ResponseWriter
+    Res http.ResponseWriter
+    Req *http.Request
+    Params MatchMap
+    Next func()
+}
+
+type Web struct {
     route map[string]Handle
-    middleware []MiddlewareHandle
+    middleware []Handle
     patternAry [][]string
     patternRegx regexp.Regexp
 }
 
-type Web struct {
-    mux Mux
-}
+/* private method */
 
-/* method define */
+func (web *Web)ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-func (mux *Mux)ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    matchMap, pattern := web.match(r.URL.Path)
 
-    // r.URL.Path = "/comment/abc/nihao"
+    ctx := Context{w, w, r, matchMap, nil}
 
-    matchMap, pattern := mux.Match(r.URL.Path)
-
-    res := Response{w, matchMap}
-    req := Request{*r}
-
-    for _, preHandle := range mux.middleware {
+    for _, preHandle := range web.middleware {
         keep := false
-        preHandle(res, &req, func() {
+        ctx.Next = func() {
             keep = true
-        })
+        }
+        preHandle(ctx)
         if !keep {
             return
         }
     }
 
-    handle, ok := mux.route[r.Method + ":" + pattern]
+    fmt.Println(pattern)
+
+    handle, ok := web.route[r.Method + ":" + pattern]
 
     if ok {
-        handle(res, &req)
+        handle(ctx)
     } else {
         http.NotFound(w, r)
     }
 
 }
 
-func (mux *Mux)Match(path string) (matchMap MatchMap, pattern string) {
+func (web *Web)match(path string) (matchMap MatchMap, pattern string) {
 
-    pathAry := mux.patternRegx.FindAllString(path, -1)
+    pathAry := web.patternRegx.FindAllString(path, -1)
     matchMap = make(map[string]string)
 
-    for _, patternItem := range mux.patternAry {
-        for i, patternKey := range patternItem {
-            if i > len(pathAry) - 1 {
+    for _, patternItem := range web.patternAry {
+        for j, patternKey := range patternItem {
+            if j > len(pathAry) - 1 {
                 break
             }
-            pathKey := pathAry[i]
+            pathKey := pathAry[j]
             if strings.HasPrefix(patternKey, ":") {
                 name := strings.TrimPrefix(patternKey, ":")
                 matchMap[name] = pathKey
@@ -85,69 +79,79 @@ func (mux *Mux)Match(path string) (matchMap MatchMap, pattern string) {
                     break
                 }
             }
+            // match success
+            if j == len(patternItem) - 1 {
+                pattern = strings.Join(patternItem, "/")
+                return
+            }
         }
-        pattern = strings.Join(patternItem, "/")
     }
 
     return
 }
 
-func (mux *Mux)Add(pattern string) {
-    mux.patternAry = append(mux.patternAry, strings.Split(pattern, "/"))
+func (web *Web)add(pattern string) {
+    web.patternAry = append(web.patternAry, strings.Split(pattern, "/"))
 }
+
+func (web *Web)addHandle(method string, pattern string, handle Handle) {
+    web.route[method + ":" + pattern] = handle
+    web.add(pattern)
+}
+
+/* public api */
 
 func (web *Web)New() {
-    web.mux = Mux{}
-    web.mux.route = make(map[string]Handle)
-    web.mux.middleware = []MiddlewareHandle{}
-    web.mux.patternAry = make([][]string, 0)
+    web.route = make(map[string]Handle)
+    web.middleware = []Handle{}
+    web.patternAry = make([][]string, 0)
     patternRegx, _ := regexp.Compile("([^/])*")
-    web.mux.patternRegx = *patternRegx
+    web.patternRegx = *patternRegx
 }
 
-func (web *Web)Listen(addr string, port int) {
-    http.ListenAndServe(addr + ":" + strconv.Itoa(port), &web.mux)
+func (web *Web)Listen(addr string) {
+    http.ListenAndServe(addr, web)
 }
 
-func (web *Web)Use(handle MiddlewareHandle) {
-    web.mux.middleware = append(web.mux.middleware, handle)
-}
-
-func (web *Web)AddHandle(method string, pattern string, handle Handle) {
-    web.mux.route[method + ":" + pattern] = handle
-    web.mux.Add(pattern)
+func (web *Web)Use(handle Handle) {
+    web.middleware = append(web.middleware, handle)
 }
 
 func (web *Web)Get(pattern string, handle Handle) {
-    web.AddHandle("GET", pattern, handle)
+    web.addHandle("GET", pattern, handle)
 }
 
 func (web *Web)Post(pattern string, handle Handle) {
-    web.AddHandle("POST", pattern, handle)
+    web.addHandle("POST", pattern, handle)
 }
 
 /* test case */
 
-func Cors(w Response, r *Request, next func()) {
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    next()
+func Cors(ctx Context) {
+    ctx.Header().Set("Access-Control-Allow-Origin", "*")
+    ctx.Next()
 }
 
-func Abc(w Response, r *Request, next func()) {
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    next()
+func Static(root string) func(ctx Context) {
+    return func(ctx Context) {
+        fileName := root + ctx.Req.URL.Path
+        if f, err := os.Stat(fileName); err == nil && !f.IsDir() {
+            http.ServeFile(ctx.Res, ctx.Req, fileName)
+        } else {
+            ctx.Next()
+        }
+    }
 }
 
-func Index(w Response, r *Request) {
-    fmt.Println(w.parms)
-    w.Write([]byte("你好"))
+func Index(ctx Context) {
+    ctx.Write([]byte("nihao"))
 }
 
 func main() {
     web := Web{}
     web.New()
     web.Use(Cors)
-    web.Use(Abc)
+    web.Use(Static("public"))
     web.Get("/comment/:id/:s", Index)
-    web.Listen("0.0.0.0", 9090)
+    web.Listen("0.0.0.0:9090")
 }
